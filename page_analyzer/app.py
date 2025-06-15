7cd000d5072277171321951660ff53fde0247c9a
Sun-Austerlitz <sankibaev.asqar@gmail.com>
2025-06-15 19:03:41 +0500

    step 4: Checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -42,15 +42,29 @@ def urls():
                     url_id = cur.fetchone()
                     if url_id:
                         flash("URL added successfully!", "success")
+                        return redirect(url_for("show_url", id=url_id[0]))
                     else:
+                        cur.execute(
+                            "SELECT id FROM urls WHERE name = " "%s", (normalized_url,)
+                        )
+                        existing_url_id = cur.fetchone()
                         flash("URL already exists.", "info")
+                        return redirect(url_for("show_url", id=existing_url_id[0]))
         except Exception as e:
             flash(f"Database error: {e}", "danger")
         return redirect(url_for("urls"))
 
     with get_db_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM urls ORDER BY created_at DESC")
+            cur.execute(
+                """
+                SELECT urls.*, MAX(url_checks.created_at) AS last_check
+                FROM urls
+                LEFT JOIN url_checks ON urls.id = url_checks.url_id
+                GROUP BY urls.id
+                ORDER BY urls.created_at DESC
+                """
+            )
             urls = cur.fetchall()
     return render_template("urls.html", urls=urls)
 
@@ -61,7 +75,30 @@ def show_url(id):
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
             url = cur.fetchone()
+            cur.execute(
+                "SELECT id, created_at FROM url_checks WHERE url_id = "
+                "%s ORDER BY created_at DESC",
+                (id,),
+            )
+            checks = cur.fetchall()
     if not url:
         flash("URL not found.", "danger")
         return redirect(url_for("urls"))
-    return render_template("url.html", url=url)
+    return render_template("url.html", url=url, checks=checks)
+
+
+@app.route("/urls/<int:id>/checks", methods=["POST"])
+def create_check(id):
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO url_checks (url_id) VALUES "
+                    "(%s) RETURNING id, created_at",
+                    (id,),
+                )
+                check = cur.fetchone()
+                flash("Check created successfully!", "success")
+    except Exception as e:
+        flash(f"Database error: {e}", "danger")
+    return redirect(url_for("show_url", id=id))
